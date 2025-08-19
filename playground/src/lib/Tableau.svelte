4d2bc3fe84bdec154a369fa340e54e8e696766ba
odilf <odysseas.maheras@gmail.com>
2025-08-19 10:35:51 +0300

    feat: compress tableaux for single children

--- playground/src/lib/Tableau.svelte
+++ playground/src/lib/Tableau.svelte
@@ -4,28 +4,53 @@
 
 	let {
 		tableau = $bindable(),
-		// FIXME: we should get the initial values from the parent or something.
-		// Right now, the height does't really matter and the width is just `--container-xl`
-		// from tailwind, which is a bit bodgy.
 		width,
 		height,
 		editable = false,
-		minLineHeight = 40,
-		margin = { top: 40, right: 40, bottom: 40, left: 40 }
+		margin = { top: 20, right: 20, bottom: 20, left: 20 },
+		crossOffset = 20,
+		minLineHeight = 20,
+		splitHeight = 35,
+		linePadding = { top: 12, bottom: 14 }
 	}: {
 		tableau: Tableau;
 		width: number;
 		height?: number;
 		editable?: boolean;
-		minLineHeight?: number;
 		margin?: { top: number; right: number; bottom: number; left: number };
+		/** How much space between the leaf nodes and the crosses if they're dead */
+		crossOffset?: number;
+		/** Minimum line height for line + split, in case `height` is undefined or too small */
+		minLineHeight?: number;
+		splitHeight?: number;
+		/**
+		 * How much extra space to leave between the top and bottom of lines that connect node.
+		 * If this were 0 then the lines would come out of the center of nodes, which is undesirable.
+		 */
+		linePadding?: { top: number; bottom: number };
 	} = $props();
 
-	const actualHeight = $derived(
-		Math.max(height ?? 0, tableau.depth() * minLineHeight + margin.top + margin.bottom)
+	const d3Height = $derived(
+		Math.max(
+			height ?? 0,
+			tableau.depth() * (minLineHeight + splitHeight) + margin.top + margin.bottom
+		)
 	);
 
-	let root = $derived(
+	const actualHeight = $derived.by(() => {
+		const heightOf = (node: number): number => {
+			const children = tableau.children(node);
+			if (children.length === 1) {
+				return heightOf(children[0]) + minLineHeight;
+			} else {
+				return (d3.max(Array.from(children).map(heightOf)) ?? 0) + minLineHeight + splitHeight;
+			}
+		};
+
+		return Math.max(heightOf(tableau.root()) + margin.top + margin.bottom, height ?? 0);
+	});
+
+	let d3root = $derived(
 		d3.hierarchy({ value: tableau.get(tableau.root()), id: tableau.root() }, (node) =>
 			Array.from(tableau.children(node.id)).map((child) => ({
 				value: tableau.get(child),
@@ -34,12 +59,10 @@
 		)
 	);
 
-	const linePadding = { top: 10, bottom: 12 };
-
 	let d3Tree = $derived(
 		d3
 			.tree<{ value: Node; id: number }>()
-			.size([width - margin.left - margin.right, actualHeight - margin.top - margin.bottom])(root)
+			.size([width - margin.left - margin.right, d3Height - margin.top - margin.bottom])(d3root)
 	);
 </script>
 
@@ -61,7 +84,7 @@
 	}}
 	<text
 		{x}
-		{y}
+		y={y + yOffset}
 		font-weight={isDead ? 'normal' : 'bold'}
 		onclick={inferCurrent}
 		onkeydown={(event) => {
@@ -78,10 +101,22 @@
 	{#if children !== undefined}
 		<!-- eslint-disable-next-line svelte/require-each-key -->
 		{#each children as child}
-			{@render tree(child, yOffset)}
+			{#if children.length > 1}
+				<line
+					stroke="currentColor"
+					stroke-width="1pt"
+					stroke-linecap="round"
+					x1={x}
+					y1={y + yOffset + linePadding.top}
+					x2={child.x}
+					y2={child.y + yOffset - linePadding.bottom}
+				/>
+			{/if}
+
+			{@render tree(child, children.length === 1 ? yOffset - splitHeight : yOffset)}
 		{/each}
 	{:else if isDead}
-		<text {x} y={y + 20}> ╳ </text>
+		<text {x} y={yOffset + y + crossOffset}> ╳ </text>
 	{/if}
 {/snippet}
 
@@ -89,19 +124,6 @@
 	<g transform="translate({margin.left}, {margin.top})" fill="currentColor">
 		<g text-anchor="middle" dominant-baseline="middle">
 			{@render tree(d3Tree)}
-		</g>
-
-		<g stroke="currentColor" stroke-width="1pt" stroke-linecap="round">
-			{#each root.links() as link (link)}
-				{#if Math.abs((link.source.x ?? 0) - (link.target.x ?? 0)) > 0.01}
-					<line
-						x1={link.source.x}
-						y1={(link.source.y ?? 0) + linePadding.top}
-						x2={link.target.x}
-						y2={(link.target.y ?? 0) - linePadding.bottom}
-					/>
-				{/if}
-			{/each}
 		</g>
 	</g>
 </svg>
