246ba54a298f07bfd1b7bf8aae6df51bdfdb338a
odilf <odysseas.maheras@gmail.com>
2025-08-19 09:43:30 +0300

    feat: add logic symbols

---
+++ playground/src/lib/Concludes.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import { Logic } from '$rust/tableaux';
+
+	const { holds = false, logic = Logic.classical() } = $props();
+
+	const inferSymbol = holds ? '⊢' : '⊬';
+</script>
+
+{inferSymbol} <sub class="inline-block -translate-x-1 text-[0.7rem]"> {logic.symbol()} </sub>
